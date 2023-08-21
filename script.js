52c54e0f494d1f8b000bc1f7f2728618dbd1a8af
Claudio Marques <claudiomarquesprogramador@gmail.com>
2023-08-21 20:29:24 -0300

    update

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-function goToElement(elementId) {
-    const element = document.getElementById(elementId);
-    if (element) {
-        element.scrollIntoView({ behavior: 'smooth' });
+//toggle colors
+const modeToggle = document.getElementById('mode-toggle');
+const body = document.body;
+
+modeToggle.addEventListener('click', () => {
+    if (body.classList.contains('light')) {
+        body.classList.remove('light');
+        body.classList.add('dark');
+    } else {
+        body.classList.remove('dark');
+        body.classList.add('light');
     }
-}
+});
+
+//go to element
+const sections = document.querySelectorAll('section');
+const upButtons = document.querySelectorAll('.up-button');
+const downButtons = document.querySelectorAll('.down-button');
+
+upButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        if (index > 0) {
+            sections[index - 1].scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+    });
+});
+
+downButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        if (sections[index + 1]) {
+            sections[index + 1].scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+    });
+});
